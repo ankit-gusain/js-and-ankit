169398e9e71df20d68ab7b917aba031027e8c311
ankit singh <108168040+ankit-gusain@users.noreply.github.com>
2024-02-14 10:02:26 +0000

    changes done

---
+++ 03/03_functions.js
@@ -0,0 +1,26 @@
+//object and functions
+let student1 = {
+    username: "sam",
+    gender: "male",
+    age: 34
+}
+
+function display(anyobj) {
+    return `student with name ${anyobj.username} with gender ${anyobj.gender} has age ${anyobj.age}`;
+
+}
+
+console.log(display(student1));
+
+//arrays and function
+let student2 = [
+    sname = "gamer",
+    gender = "male"
+
+]
+
+function disp(getArray) {
+    return `student ${student2[0]} has gender ${student2[1]}`;
+}
+
+console.log(disp(student2));
